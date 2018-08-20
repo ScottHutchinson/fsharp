467f5294a9730f31e9094b8f771c5c5c64dd4b99
Scott Hutchinson <scotthutchinson@usa.net>
2018-08-19 20:43:20 -0700

    Added List.fold to complete the fix (?).

--- src/fsharp/TypeChecker.fs
+++ src/fsharp/TypeChecker.fs
@@ -15690,9 +15690,10 @@ module EstablishTypeDefinitionCores =
             and accStructFieldType structTycon structTyInst fspec fieldTy (doneTypes, acc) =
                 let fieldTy = stripTyparEqns fieldTy
                 match fieldTy with
-                | TType_tuple (_isStruct , t2) when isStructTupleTy cenv.g fieldTy ->
-                    // The field is a struct tuple. Decapitate and repeat.
-                    accStructFieldType structTycon structTyInst fspec (List.head t2) (doneTypes, acc)
+                | TType_tuple (_isStruct , tinst2) when isStructTupleTy cenv.g fieldTy ->
+                    // The field is a struct tuple. Check each element of the tuple.
+                    // This case was added to resolve https://github.com/Microsoft/visualfsharp/issues/3916
+                    tinst2 |> List.fold (fun acc' x -> accStructFieldType structTycon structTyInst fspec x acc') (doneTypes, acc)
                 | TType_app (tcref2 , tinst2) when tcref2.IsStructOrEnumTycon ->
                     // The field is a struct.
                     // An edge (tycon, tycon2) should be recorded, unless it is the "static self-typed field" case.
